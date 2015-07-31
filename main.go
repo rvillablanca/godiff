2770251df0d291fa10592c763ddaa80a584838ce
Rodrigo Villablanca <villa061004@gmail.com>
2015-07-31 16:47:16 -0300

    Avances

--- main.go
+++ main.go
@@ -5,13 +5,31 @@ import (
 	"github.com/rvillablanca/godiff/diff"
 	"os"
 	"log"
+	"errors"
 )
 
 func main() {
-	fmt.Println("Argumentos:", os.Args)
+	args := os.Args
+	err := checkArguments(args)
+	if err != nil {
+		printUsages()
+		return
+	}
 	result, err := diff.CompareFiles(os.Args[1], os.Args[2])
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("Resultado", result)
+}
+
+func checkArguments(args []string) error {
+	if (len(args) != 4) {
+		return errors.New("Número de parámetros incorrecto")
+	}
+	return nil
+}
+
+func printUsages() {
+	fmt.Println("Uso: ")
+	fmt.Println(os.Args[0], "<old-sources> <new-sources> <destination>")
 }
