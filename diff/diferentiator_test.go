e228b9878afaa9522ad63e4403bc39458f1e9264
Rodrigo Villablanca <villa061004@gmail.com>
2015-08-01 17:54:23 -0300

    Fix

--- diff/diferentiator_test.go
+++ diff/diferentiator_test.go
@@ -4,10 +4,9 @@ import "testing"
 import "fmt"
 
 func TestWalk(t *testing.T) {
-  fmt.Println("Ejectando test walk...")
+  fmt.Println("Ejecutando test walk...")
   list := []string{}
   list = FindFilesIn(list, "/home/mechon/go")
-  fmt.Println("Archivos:", list)
   if (len(list) == 0) {
     t.Error("Error en test walk")
   }
