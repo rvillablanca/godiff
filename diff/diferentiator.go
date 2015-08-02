313117f47f078ae036108b95089ea0d4322b8a8c
Rodrigo Villablanca <villa061004@gmail.com>
2015-08-02 20:52:19 -0300

    Avances

--- diff/diferentiator.go
+++ diff/diferentiator.go
@@ -6,21 +6,6 @@ import (
 	"path/filepath"
 )
 
-func CheckDirectory(dirname string) (result bool, err error) {
-	f, err := os.Open(dirname)
-	if err != nil {
-		return
-	}
-	defer f.Close()
-	fi, err := f.Stat()
-	if err != nil {
-		return
-	}
-
-	result = fi.Mode().IsDir()
-	return
-}
-
 func CompareFiles(file1, file2 string) (result bool, err error) {
 	size := 1024 * 8
 	buffer1, buffer2 := make([]byte, size), make([]byte, size)
--- main.go
+++ main.go
@@ -5,61 +5,77 @@ import (
 	"github.com/rvillablanca/godiff/diff"
 	"os"
 	"path/filepath"
+	"errors"
 )
+
+type diffconf struct {
+	oldDir string
+	newDir string
+	destDir string
+}
 
 func main() {
 	fmt.Println("Verificando parámetros...")
-	args := os.Args
-	valid := checkArguments(args)
-	if !valid {
-		fmt.Println("Número de parámetros incorrecto")
-		printUsages()
-		return
-	}
-	dir1 := os.Args[1]
-	dir2 := os.Args[2]
 
-	dir1, err := filepath.Abs(dir2)
+	conf, err := validateArguments(os.Args)
 	if err != nil {
-		fmt.Println("No fue posible verificar el directorio", dir1)
-	}
-	dir2, err = filepath.Abs(dir1)
-	if err != nil {
-		fmt.Println("No fue posible verificar el directorio", dir2)
-	}
-
-	valid, err = checkDirectories(dir1, dir2)
-	if err != nil {
-		fmt.Println("No fue posible verificar los directorios")
+		fmt.Println(err)
 		return
 	}
 
-	if !valid {
-		fmt.Println("Todos los argumentos deben ser directorios")
-		return
-	}
-
-	fmt.Println("Versión anterior:", dir1)
-	fmt.Println("Versión nueva:", dir2)
+	fmt.Println("Versión anterior:", conf.oldDir)
+	fmt.Println("Versión nueva:", conf.newDir)
 
 	oldFiles := []string{}
 	newFiles := []string{}
 	fmt.Println("Buscando archivos en directorios...")
-	oldFiles = diff.FindFilesIn(oldFiles, dir1)
-	newFiles = diff.FindFilesIn(newFiles, dir2)
+	oldFiles = diff.FindFilesIn(oldFiles, conf.oldDir)
+	newFiles = diff.FindFilesIn(newFiles, conf.newDir)
 
 }
 
-func checkArguments(args []string) bool {
-	return len(args) == 4
+func validateArguments(args []string) (conf diffconf, err error) {
+	valid := len(args) == 4
+	if !valid {
+		err = errors.New("Número de parámetros incorrecto")
+		return
+	}
+
+	dir1 := args[1]
+	dir2 := args[2]
+	dir3 := args[3]
+
+	dir1, err = filepath.Abs(dir2)
+	if err != nil {
+		err = errors.New("No fue posible verificar el directorio" + dir1)
+		return
+	}
+	dir2, err = filepath.Abs(dir1)
+	if err != nil {
+		err = errors.New("No fue posible verificar el directorio" + dir2)
+		return
+	}
+
+	valid, err = checkDirectories(dir1, dir2)
+	if err != nil {
+		err = errors.New("No fue posible verificar los directorios")
+		return
+	}
+
+	if !valid {
+		err = errors.New("Todos los argumentos deben ser directorios")
+		return
+	}
+
+	return diffconf{dir1, dir2, dir3}, nil
 }
 
 func checkDirectories(dir1, dir2 string) (bool, error) {
-	isDir1, err := diff.CheckDirectory(dir1)
+	isDir1, err := checkDirectory(dir1)
 	if err != nil {
 		return false, err
 	}
-	isDir2, err := diff.CheckDirectory(dir2)
+	isDir2, err := checkDirectory(dir2)
 	if err != nil {
 		return false, err
 	}
@@ -67,6 +83,21 @@ func checkDirectories(dir1, dir2 string) (bool, error) {
 		return false, nil
 	}
 	return true, nil
+}
+
+func checkDirectory(dirname string) (result bool, err error) {
+	f, err := os.Open(dirname)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+	fi, err := f.Stat()
+	if err != nil {
+		return
+	}
+
+	result = fi.Mode().IsDir()
+	return
 }
 
 func printUsages() {
