80cfa50defaa715759fa91a9e8a935210a6fc4b4
Rodrigo Villablanca <villa061004@gmail.com>
2015-08-28 15:12:05 -0300

    fix

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"io"
 
 	"github.com/rvillablanca/godiff/diff"
 	"gopkg.in/alecthomas/kingpin.v2"
@@ -94,6 +95,26 @@ loop:
 		}
 	}
 	fmt.Println("Reemplazar:", toReplace)
+
+  for _, file := range toAdd {
+		fmt.Println("Copiando nuevo archivo: ", file)
+		err = CopyFile(filepath.Join(*newDir, file), filepath.Join(*destDir, file))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
+	for _, file := range toReplace {
+		fmt.Println("Reemplazando archivo: ", file)
+		err = CopyFile(file, filepath.Join(*destDir, file))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
+	fmt.Println("Finalizado")
 }
 
 func validateDirectories() (err error) {
@@ -146,4 +167,57 @@ func checkDirectory(dirname string) (result bool, err error) {
 
 	result = fi.Mode().IsDir()
 	return
+}
+
+// CopyFile copies a file from src to dst. If src and dst files exist, and are
+// the same, then return success. Otherise, attempt to create a hard link
+// between the two files. If that fail, copy the file contents from src to dst.
+func CopyFile(src, dst string) (err error) {
+    sfi, err := os.Stat(src)
+    if err != nil {
+        return
+    }
+    if !sfi.Mode().IsRegular() {
+        // cannot copy non-regular files (e.g., directories,
+        // symlinks, devices, etc.)
+        return fmt.Errorf("CopyFile: non-regular source file %s (%q)", sfi.Name(), sfi.Mode().String())
+    }
+    dfi, err := os.Stat(dst)
+    if err != nil {
+        if !os.IsNotExist(err) {
+            return
+        }
+    } else {
+        if !(dfi.Mode().IsRegular()) {
+            return fmt.Errorf("CopyFile: non-regular destination file %s (%q)", dfi.Name(), dfi.Mode().String())
+        }
+        if os.SameFile(sfi, dfi) {
+            return
+        }
+    }
+    err = copyFileContents(src, dst)
+    return
+}
+
+func copyFileContents(src, dst string) (err error) {
+    in, err := os.Open(src)
+    if err != nil {
+        return
+    }
+    defer in.Close()
+    out, err := os.Create(dst)
+    if err != nil {
+        return
+    }
+    defer func() {
+        cerr := out.Close()
+        if err == nil {
+            err = cerr
+        }
+    }()
+    if _, err = io.Copy(out, in); err != nil {
+        return
+    }
+    err = out.Sync()
+    return
 }
